274b27c67c58fc6904cd4c6f807406670bd08090
Matthew Rodriguez (TATA CONSULTANCY SERVICES LTD) <v-matthewro@microsoft.com>
2022-11-11 14:40:10 -0800

    backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL:'http://localhost:5000'}); //backend host  {baseURL:'http://localhost:5000'}
+const API = axios.create({baseURL:'https://hackkathon22.herokuapp.com/'}); //backend host  {baseURL:'http://localhost:5000'}
 
 export const visitorSignIn = (emp) => API.post('/',emp)
