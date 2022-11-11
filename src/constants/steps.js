77ca14d9c860bbedbb7cfab6e2bae4b70187a8d9
Matthew Rodriguez (TATA CONSULTANCY SERVICES LTD) <v-matthewro@microsoft.com>
2022-11-11 12:53:30 -0800

    chatbot

--- src/constants/steps.js
+++ src/constants/steps.js
@@ -2,8 +2,24 @@
 export const steps = [
     {
         id: '0',
-        message: 'Hey Geek!',
-        end: true
+        message: 'Hello!',
+        trigger: '1'
+    }, {
+        id: '1',
+        // This message appears in
+        // the bot chat bubble
+        message: 'What is your name?',
+        trigger: '2'
+    }, {
+        id: '2',
+        // Here we want the user
+        // to enter input
+        user: true,
+        trigger: '3',
+    }, {
+        id: '3',
+        message: " hi {previousValue}, how can I help you?",
+        //trigger: '4'
     }
 ];
 
